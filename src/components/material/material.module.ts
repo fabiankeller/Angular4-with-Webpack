da3b8b3986205287492cd273b0fc25222a92d3ed
Fabian Keller <fabk@zuehlke.com>
2017-11-10 10:26:20 +0100

    Create basic layout of app

--- src/components/material/material.module.ts
+++ src/components/material/material.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material'
 import { MatToolbarModule } from '@angular/material'
 import { MatMenuModule } from '@angular/material'
+import { MatTabsModule } from '@angular/material'
+import { MatSidenavModule } from '@angular/material'
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
     declarations: [],
     exports: [
         MatCheckboxModule,
         MatToolbarModule,
-        MatMenuModule
+        MatMenuModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: []
 })
 
-export class MaterialModule {}
+export class MaterialModule { }
